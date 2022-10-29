7becf4b699dbaacb3e06b09e92da3eeeab1f1e33
Yael Palman <ranbroda@gmail.com>
2022-10-29 10:22:08 +0300

    Add about text and delete 'from Java' message from weather screen

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,6 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-messageOne.textContent = 'From JavaScript'
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -13,7 +12,7 @@ weatherForm.addEventListener('submit', (event) => {
     messageTwo.textContent = ''
 
     const location = search.value
-    
+
     fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {   
         if (data.error) {
